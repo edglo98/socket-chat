214b5ea5bb03c1ad20599f851a13465712726bf9
Edgar Lopez <edgarlopezmgs@live.com>
2021-10-09 23:28:46 -0500

    add: validation JWT in frontend

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,2 +1,31 @@
+/* global localStorage fetch */
+const url = window.location.hostname.includes('localhost')
+  ? 'http://localhost:8080/api/auth/'
+  : 'https://cafe-api-e.herokuapp.com/api/auth/'
+
+let user = null
+const socket = null
+
+const validateJWT = async () => {
+  const token = localStorage.getItem('token') || ''
+
+  if (token.length <= 10) {
+    throw new Error('no hay token')
+  }
+
+  const resp = await fetch(url, {
+    headers: { 'x-token': token }
+  })
+
+  const data = await resp.json()
+  localStorage.setItem('token', data.token)
+  user = data.user
+}
+
+const main = async () => {
+  await validateJWT()
+}
+
+main()
 // eslint-disable-next-line no-undef
-const socket = io()
+// const socket = io()
